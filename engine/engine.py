5ce3d845997fb0592590c838df047bb6a70276d9
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-01-27 00:43:36 +0200

    Modified engine class and removed unused classes

--- engine/engine.py
+++ engine/engine.py
@@ -3,12 +3,23 @@ from engine.renderer.renderer import Renderer
 from engine.input_handler.input_handler import InputHandler
 
 class Engine:
-    """Engine class is the main wrapper of the pygame based game engine. Contains main loop"""
+    """Engine class is the main wrapper of the pygame based game engine. 
+        All engine functionality initialized here, also runs main loop"""
     def __init__(self) -> None:
         pygame.init()
         self.__renderer = Renderer()
         self.__input_handler = InputHandler()
         self.__loop = True
+
+    @property
+    def renderer(self):
+        """Read-only reference to the renderer"""
+        return self.__renderer
+
+    @property
+    def input_handler(self):
+        """Read-only reference to the input handler"""
+        return self.__input_handler
 
     def run(self):
         """Starts the main loop"""
--- room_generator/room.py
+++
@@ -1,6 +0,0 @@
-class Room:
-    def __init__(self) -> None:
-        self.connections = None
-        self.size = 0
-        self.room_type = None
-        
