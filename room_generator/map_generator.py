9b2720f9e9d0ef7868c7f14cd0f467de04552383
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-02-27 15:28:08 +0200

    Refactored map generator

--- room_generator/map_generator.py
+++ room_generator/map_generator.py
@@ -1,81 +1,98 @@
-import time
+"""
+Module containing Map dataclass and Map generator class.
+The latter is used to generate a 2d map procedurally.
+"""
+
 import random
+from dataclasses import dataclass
 from room_generator.geometry import Point, Edge
 from room_generator.grid import Grid
 
+@dataclass
 class Map:
     """
     Container object used to store information about a generated map.
     """
-    def __init__(
-            self,
-            size : int,
-            points : list[Point],
-            edges : list[Edge],
-            minimum_spanning_tree : list[Edge],
-            map_diagram : list[Edge],
-            grid : Grid
-        ) -> None:
-        self.size = size
-        self.points = points
-        self.edges = edges
-        self.minimum_spanning_tree = minimum_spanning_tree 
-        self.map_diagram = map_diagram
-        self.grid = grid
+    size: int
+    points: list[Point]
+    triangulation: list[Edge]
+    minimum_spanning_tree: list[Edge]
+    map_diagram: list[Edge]
+    grid: Grid
 
 class MapGenerator:
     """
     Responsible for generating maps using the given generators.
 
     Args:
-        point_generator: Generator class responsible for creating
-        a random set of points over an area.
+        room_placer: Generator class responsible for creating
+        a random set of rooms in random locations.
         triangulator: Generator class responsible for creating
-        a Delaunay triangulation of the points from the point generator.
+        a Delaunay triangulation of the points from the rooms'
+        center points.
         min_tree_generator: Generator Class responsible for crating
         a Minimum spanning tree from the Delaunay triangulation.
+        room_connector: Class responsible for adding random edges
+        to the MST.
     """
-    def __init__(self, room_placer, triangulator, min_tree_generator, room_connector) -> None:
+    def __init__(
+            self,
+            room_placer,
+            triangulator,
+            min_tree_generator,
+            room_connector
+        ) -> None:
         self.room_placer = room_placer
         self.triangulator = triangulator
         self.min_tree_generator = min_tree_generator
         self.room_connector = room_connector
 
-    def generate(self, seed : int = None, size : int = 64, amount : int = 32) -> Map:
+    def generate(
+            self,
+            seed : int = None,
+            size : int = 64,
+            amount : int = 32
+        ) -> Map:
         """
-        Generates a map object using given settings.
+        Generates a map dataclass using given settings.
 
-        Args: 
+        Args:
             size: Size of the map to be generated.
-            Currently the side length of square bounds that the map is generated in.
-            amount: How many points are generated inside the bounds.
+            Currently the side length of square
+            bounds that the map is generated in.
+            amount: How many points are generated
+            inside the bounds.
 
         Returns:
-            Map object containing generated points, edges, MST, etc.
+            Map dataclass containing generated points, edges, MST, etc.
+            See Map documentation for more details.
         """
-        total_start = time.time()
         random.seed(seed)
-        print("Generating points...")
-        points = []
         grid = Grid(size)
-        rp = self.room_placer.generate_rooms(amount, grid, seed)
-        rooms = rp[1]
-        new_grid = rp[0]
-        for room in rooms:
-            points.append(room.center_point)
-        edges = []
-        print("Points generated")
-        print("Triangulating points...")
-        starttime = time.time()
-        edges = self.triangulator.triangulate_points(points)
-        endtime = time.time()
-        print(f"Creating triangulation of {len(edges)} edges for {len(points)} vertices took {endtime-starttime} seconds")
-        print("Creating Minimum Spanning Tree for triangulation...")
-        starttime = time.time()
-        mst = self.min_tree_generator.create_tree_from_edges(edges)
-        endtime = time.time()
-        print(f"Creating MST for {len(points)} vertices with {len(mst)} edges took {endtime-starttime} seconds")
-        complete_map_diagram = self.room_connector.create_connections(seed, mst, edges)
-        total_end = time.time()
-        print(f"Map took {total_end-total_start} seconds to generate")
-        return Map(size, points, edges, mst, complete_map_diagram, new_grid)
+        room_grid_tuple = self.room_placer.generate_rooms(
+            amount,
+            grid,
+            seed
+        )
+        rooms = room_grid_tuple[1]
+        new_grid = room_grid_tuple[0]
+        points = [room.center_point for room in rooms]
+        triangulation_edges = self.triangulator.triangulate_points(
+            points
+        )
+        mst = self.min_tree_generator.create_tree_from_edges(
+            triangulation_edges
+        )
+        complete_map_diagram = self.room_connector.create_connections(
+            seed,
+            mst,
+            triangulation_edges
+        )
+        return Map(
+            size,
+            points,
+            triangulation_edges,
+            mst,
+            complete_map_diagram,
+            new_grid
+        )
